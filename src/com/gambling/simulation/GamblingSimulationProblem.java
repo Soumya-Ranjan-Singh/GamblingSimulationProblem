7eab7f32fc5bc6446103916fb7ce68ba10da3782
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-20 20:27:18 +0530

    Added : Display Welcome to Gambling Simulation Program

---
+++ src/com/gambling/simulation/GamblingSimulationProblem.java
@@ -0,0 +1,19 @@
+//This program is to display the Welcome message to Gambling Simulation Program.
+
+package com.gambling.simulation;
+
+public class GamblingSimulationProblem {
+
+    //Default Constructor
+    public GamblingSimulationProblem() {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize the object.
+        GamblingSimulationProblem gambling = new GamblingSimulationProblem();
+        System.out.println();
+
+    }
+}
